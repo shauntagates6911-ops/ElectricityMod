baebed10be9c83af1c32f087dc741555e332ff9d
shauntagates6911-ops <shaunta.gates6911@gmail.com>
2026-08-20 22:12:30 -0400

    Add initial configuration for Circuit Logic Gate

---
+++ files.js
@@ -0,0 +1,23 @@
+{
+  "extension": ".lightning",
+  "version": "1.0.0",
+  "meta": {
+    "title": "Circuit Logic Gate",
+    "author": "kenny6",
+    "created": "2026-08-20T22:11:57Z"
+  },
+  "targets": [
+    {
+      "isStage": true,
+      "name": "Stage",
+      "variables": {
+        "voltage": 12,
+        "current": 2.5
+      },
+      "blocks": {}
+    }
+  ],
+  "extensions": [
+    "electricity"
+  ]
+}
